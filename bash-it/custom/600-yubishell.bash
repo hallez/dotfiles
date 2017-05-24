50897baac127bda62796c8276c9aa947429ee9d3
Rob Dimsdale-Zucker <robert.dimsdale@gmail.com>
2017-05-23 22:02:01 -0700

    Add yubishell function.

---
+++ bash-it/custom/600-yubishell.bash
@@ -0,0 +1,9 @@
+yubishell() {
+  (
+    eval $(keychain --agents "gpg" --quiet --eval);
+    gpg-connect-agent updatestartuptty /bye;
+    export SSH_AUTH_SOCK="$HOME/.gnupg/S.gpg-agent.ssh"
+    export PS1='\[\e[31m\][YUBI]\[\e[0m\] \$ ';
+    bash
+  )
+}
