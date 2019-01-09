173b5ccdcc61c3b70c6d298d1ac6fabcc59b7180
Halle Dimsdale-Zucker <hrzucker@ucdavis.edu>
2019-01-09 12:26:18 -0800

    try exporting variables

--- bash-it/custom/080-matlab.bash
+++ bash-it/custom/080-matlab.bash
@@ -4,9 +4,11 @@ set -eu
 
 CURRENT_VERSION="R2017a"
 MATLAB_PATH="/Applications/MATLAB_${CURRENT_VERSION}.app/bin/matlab"
+export CURRENT_VERSION
+export MATLAB_PATH
 
-if [[ -f "/Applications/MATLAB_${CURRENT_VERSION}.app/bin/matlab" ]]; then
-  source "/Applications/MATLAB_${CURRENT_VERSION}.app/bin/matlab"
+if [[ -f "${MATLAB_PATH}" ]]; then
+  source "${MATLAB_PATH}"
 fi
 
 PATH="${MATLAB_PATH}/bin:${PATH}"
