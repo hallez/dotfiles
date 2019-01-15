9735376835c4aee3729cab3e089db323a814416b
Halle Dimsdale-Zucker <hrzucker@ucdavis.edu>
2019-01-15 13:54:51 -0800

    allow matlab alias to load java

--- bash-it/custom/080-matlab.bash
+++ bash-it/custom/080-matlab.bash
@@ -4,6 +4,6 @@ set -eu
 
 CURRENT_VERSION="R2017a"
 
-alias matlab="/Applications/MATLAB_${CURRENT_VERSION}.app/bin/matlab -nojvm -nodesktop"
+alias matlab="/Applications/MATLAB_${CURRENT_VERSION}.app/bin/matlab -nodesktop"
 
 set +eu
