1d03b7ca820f4c73ccca2732ecf0cedddca97be8
Halle R. Dimsdale-Zucker <hrzucker@ucdavis.edu>
2019-01-30 11:33:36 -0500

    Update R memory usage in bash-it custom profile.
    - following advice from: https://stackoverflow.com/questions/51295402/r-on-macos-error-vector-memory-exhausted-limit-reached

--- bash-it/custom/150-r.bash
+++ bash-it/custom/150-r.bash
@@ -4,4 +4,8 @@ set -eu
 
 alias rstudio='open -a Rstudio'
 
+# increase the default memory
+# based on: https://stackoverflow.com/questions/51295402/r-on-macos-error-vector-memory-exhausted-limit-reached
+echo "R_MAX_VSIZE=100Gb" > $HOME/.Renviron
+
 set +eu
